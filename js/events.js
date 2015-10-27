a4c7c78de690b8b3947ddf698091f83fc3ac5ce8
hedgehog1029 <omgredstone@gmail.com>
2015-10-27 21:39:11 +0000

    Lots of updates; events page, audio player, signup/login styling.

--- js/events.js
+++ js/events.js
@@ -1,20 +1,37 @@
+var userData = {
+    "signed_in": false
+}; //again, temporary
+
 $(document).on("ready", function() {
     populate($("#events-wrapper").get(0));
 
+    $(".event").attr("data-select", 0);
+
     $(".event-btn").click(function() {
-        if ($(this).parent().attr("data-open") != 1) {
-            $(this).addClass("rotate");
-            $(this).parent().children(".event-ext-desc").slideDown();
-            $(this).parent().attr("data-open", 1);
+        if ($(this).parent().attr("data-select") == 0) {
+            if (!userData.signed_in) {
+                notifier.notify("times", "4", "Not signed in!", "nosignin_notifier", 3000);
+                return;
+            }
+
+            $(this).get(0).className = "check";
+            $(this).children("i").get(0).className = "fa fa-check-circle fa-2x"
+            $(this).parent().attr("data-select", 1);
+
+            notifier.notify("check", "4", "You're now going to this event!", "going_notif", 3000); // temporary; wait until server roundtrip
+        } else if ($(this).parent().attr("data-select") == 1) {
+            $(this).get(0).className = "cross";
+            $(this).children("i").get(0).className = "fa fa-times-circle fa-2x"
+            $(this).parent().attr("data-select", 2);
         } else {
-            $(this).removeClass("rotate");
-            $(this).parent().children(".event-ext-desc").slideUp();
-            $(this).parent().attr("data-open", 0);
+            $(this).get(0).className = "";
+            $(this).children("i").get(0).className = "fa fa-question-circle fa-2x"
+            $(this).parent().attr("data-select", 0);
         }
     });
 });
 
-var events = [{"thumb": "img/danika%20image.jpg", "title": "Test Event", "desc": "A test event.",
+var events = [{"thumb": "img/danika%20image.jpg", "title": "Test Event", "desc": "A test event, where people do the stuff and the things.",
 "ext_desc": "<li>Where: NCR Dubtrack room</li><li>When: 1st November 2015</li>"},
              {"thumb": "img/danika%20image.jpg", "title": "Test Event 2", "desc": "Another test event.", "ext_desc": "Extended description."}]; // temporary test variable
 
@@ -34,7 +51,7 @@ function populate(parent) {
         title.innerHTML = o.title;
         title.className = "event-title";
         
-        desc.innerHTML = o.desc;
+        desc.innerHTML = o.desc.substring(0, 300);
         desc.className = "event-description";
 
         ext_desc.innerHTML = o.ext_desc;
@@ -42,11 +59,12 @@ function populate(parent) {
         
         btn.className = "event-btn";
 
-        i.className = "fa fa-long-arrow-right";
+        i.className = "fa fa-question-circle fa-2x";
 
         btn.appendChild(i);
         
         div.className = "event";
+        div["data-select"] = 0;
         div.appendChild(img);
         div.appendChild(title);
         div.appendChild(desc);
--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 var tracks = [
     { "name": "Landslide", "artist": "Oh Wonder", "src": "sound/track1.mp3" },
-    { "name": "Tokyo (Subranger Remix)", "artist": "Ark Patrol", "src": "sound/track2.mp3" }
+    { "name": "Tokyo (Subranger Remix)", "artist": "Ark Patrol", "src": "sound/track2.mp3" },
+    { "name": "Koto", "artist": "CloZee", "src": "sound/track3.mp3" }
 ]; // Array of tracks to randomly select from on page load
 
+var pageData = {
+
+}; // Global data store
+
 $(document).ready(function() {
+    notifier.obj = $(".notifier"); // initialize notifier object
+
     var playerr = $("#player").get(0);
 
     playerr.onended = function() {
@@ -22,29 +29,42 @@ $(document).ready(function() {
     
     $("#mute").on("click", function(e) {
         if (volState == 2) {
-            $("#mute").addClass("fa-volume-down");
-            $("#mute").removeClass("fa-volume-off");
-            $("#mute").removeClass("fa-volume-up");
+            playerControl.vol.set(.5);
 
-            playerr.volume = 0.5;
             volState = 1;
         } else if (volState == 1) {
-            $("#mute").addClass("fa-volume-off");
-            $("#mute").removeClass("fa-volume-up");
-            $("#mute").removeClass("fa-volume-down");
+            playerControl.vol.set(0);
 
-            playerr.volume = 0;
             volState = 0;
         } else if (volState == 0) {
-            $("#mute").addClass("fa-volume-up");
-            $("#mute").removeClass("fa-volume-off");
-            $("#mute").removeClass("fa-volume-down");
+            playerControl.vol.set(1);
 
-            playerr.volume = 1;
             volState = 2;
         } else {
             volState = 2;
         }
+    });
+
+    $("#back").on("click", function(e) {
+        if (pageData.nowplaying == 0) {
+            pageData.nowplaying = tracks.length;
+        }
+
+        pageData.nowplaying = pageData.nowplaying - 1;
+
+        playerControl.load(pageData.nowplaying);
+        playerControl.play();
+    });
+
+    $("#forward").on("click", function(e) {
+        if (pageData.nowplaying == tracks.length - 1) {
+            pageData.nowplaying = -1;
+        }
+
+        pageData.nowplaying = pageData.nowplaying + 1;
+
+        playerControl.load(pageData.nowplaying);
+        playerControl.play();
     });
 
     // Nav cursor hover thingy
@@ -64,7 +84,9 @@ $(document).ready(function() {
 
         if (percent > 100) percent = 100;
 
-        $("#u1").css("background-position", "0% " + percent + "%");
+        var hPercent = ($(window).scrollTop() / $(window).height()) * 100;
+
+        $("#u1").css("background-position", "0% " + hPercent + "%");
         $(".links").css("background-position", "0% " + percent + "%");
 
         // Back to top button
@@ -88,6 +110,25 @@ $(document).ready(function() {
         }, 1100);
     });
 
+    // Nav login/register
+    $("#register").click(function() {
+        pageData.sso_mode = "register";
+
+        $("#sso-mode").html("Register: ");
+        $("body .user").addClass("show");
+    });
+
+    $("#login").click(function() {
+        pageData.sso_mode = "login";
+
+        $("#sso-mode").html("Login: ");
+        $("body .user").addClass("show");
+    });
+
+    $(".page").click(function() {
+        $("body .user").removeClass("show");
+    });
+
     // Back to top
     $(".btt").click(function() {
         $(".btt").removeClass("shown");
@@ -96,19 +137,27 @@ $(document).ready(function() {
             scrollTop: 0
         }, 1000);
     });
+
+    // Sekrit volume control keypress
+    $(document).keypress(function(e) {
+        if (e.which == 105) {
+            e.preventDefault();
+
+            playerControl.vol.up();
+        } else if (e.which == 107) {
+            e.preventDefault();
+
+            playerControl.vol.down();
+        }
+    })
 });
 
 $(window).load(function() {
-    var track = tracks[Math.floor(Math.random() * tracks.length)];
-    var playerr = $("#player").get(0);
+    var track = Math.floor(Math.random() * tracks.length);
 
-    playerr.src = track.src;
-    playerr.load();
-
+    pageData.nowplaying = track;
+    playerControl.load(track);
     playerControl.play();
-
-    $("#nowplaying").html(track.name + " - " + track.artist);
-    $("#nowplaying").slideDown();
 });
 
 var playerControl = {
@@ -121,5 +170,122 @@ var playerControl = {
         $("#player").get(0).pause();
         $("#play").addClass("fa-play");
         $("#play").removeClass("fa-pause");
+    },
+    "load": function(index) {
+        var playerr = $("#player").get(0);
+        var track = tracks[index];
+
+        playerr.src = track.src;
+        playerr.load();
+
+        $("#nowplaying").html(track.name + " - " + track.artist);
+        $("#nowplaying").slideDown();
+    },
+    "vol": {
+        "down": function() {
+            var playerr = $("#player").get(0);
+
+            if (playerr.volume == 0) {
+                return;
+            } else {
+                playerr.volume -= .1;
+            }
+
+            if (playerr.volume < .6 && playerr.volume > 0) {
+                $("#mute").addClass("fa-volume-down");
+                $("#mute").removeClass("fa-volume-off");
+                $("#mute").removeClass("fa-volume-up");
+            } else if (playerr.volume < .1) {
+                $("#mute").addClass("fa-volume-off");
+                $("#mute").removeClass("fa-volume-up");
+                $("#mute").removeClass("fa-volume-down");
+            }
+
+            notifier.notify("volume-up", "4", "Volume now " + Math.floor(playerr.volume * 100) + "%.", "vol_notif", 1500);
+        },
+        "up": function() {
+            var playerr = $("#player").get(0);
+
+            if (playerr.volume == 1) {
+                return;
+            } else {
+                playerr.volume += .1;
+            }
+
+            if (playerr.volume > .5) {
+                $("#mute").addClass("fa-volume-up");
+                $("#mute").removeClass("fa-volume-off");
+                $("#mute").removeClass("fa-volume-down");
+            }
+
+            notifier.notify("volume-up", "4", "Volume now " + Math.floor(playerr.volume * 100) + "%.", "vol_notif", 1500);
+        },
+        "set": function(num) {
+            var playerr = $("#player").get(0);
+
+            if (num > 1 || num < 0)
+                return;
+
+            playerr.volume = num;
+
+            if (playerr.volume > .5) {
+                $("#mute").addClass("fa-volume-up");
+                $("#mute").removeClass("fa-volume-off");
+                $("#mute").removeClass("fa-volume-down");
+
+                notifier.notify("volume-up", "4", "Volume now " + Math.floor(playerr.volume * 100) + "%.", "vol_notif", 1500);
+            } else if (playerr.volume < .6 && playerr.volume > 0) {
+                $("#mute").addClass("fa-volume-down");
+                $("#mute").removeClass("fa-volume-off");
+                $("#mute").removeClass("fa-volume-up");
+
+                notifier.notify("volume-down", "4", "Volume now " + Math.floor(playerr.volume * 100) + "%.", "vol_notif", 1500);
+            } else if (Math.floor(playerr.volume) == 0) {
+                $("#mute").addClass("fa-volume-off");
+                $("#mute").removeClass("fa-volume-up");
+                $("#mute").removeClass("fa-volume-down");
+
+                notifier.notify("volume-off", "4", "Volume now " + Math.floor(playerr.volume * 100) + "%.", "vol_notif", 1500);
+            }
+        }
+    }
+};
+
+var notifier = {
+    "obj": null,
+    "timeout": 0,
+    "notify": function(icon, icon_size, msg, id, timeout) {
+        var i = notifier.util.createIcon(icon, icon_size),
+            t = notifier.util.createText(msg, id);
+
+        notifier.obj.html("");
+
+        notifier.obj.get(0).appendChild(i);
+        notifier.obj.get(0).appendChild(t);
+
+        notifier.obj.fadeIn(500);
+
+        clearTimeout(notifier.timeout);
+
+        notifier.timeout = setTimeout(function() {
+            notifier.obj.fadeOut(500);
+        }, timeout);
+    },
+    "util": {
+        "createIcon": function(icon, size) {
+            var i = document.createElement("i");
+
+            i.className = "fa fa-" + icon + " fa-" + size + "x";
+
+            return i;
+        },
+        "createText": function(text, id) {
+            var t = document.createElement("p");
+
+            t.id = id;
+            t.innerHTML = text;
+
+            return t;
+        }
     }
 };
